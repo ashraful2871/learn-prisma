20354ddc0afa895543bc48df8b6a347a12dc3c55
ashraful2871 <ashrafulislam2758@gmail.com>
2025-09-12 06:19:55 +0600

    get data

--- index.ts
+++ index.ts
@@ -3,15 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.user.create({
-    data: {
-      name: "ash",
-      email: "ash@gmail.com",
-      profilePhoto: "https://example.com/photo.jpg", 
+  // const result = await prisma.user.create({
+  //   data: {
+  //     name: "ash",
+  //     email: "ash@gmail.com",
+  //     profilePhoto: "https://example.com/photo.jpg",
+  //   },
+  // });
+
+  // console.log(result);
+
+  // const userData=await prisma.user.findMany({
+  //   where: {
+  //    name:"ash"
+  //   }
+  // })
+  // console.log(userData)
+  const findUserById = await prisma.user.findFirstOrThrow({
+    where: {
+      id: 10,
     },
   });
-
-  console.log(result);
+  console.log(findUserById);
 }
 
 main();
